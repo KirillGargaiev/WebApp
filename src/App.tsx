78c23d65ad37d8daf245d6e184907510fc83f586
kyryllgargaiev <kg@codefinity.com>
2024-06-02 16:46:22 +0300

    feat: added react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,16 @@ import QuizPage from './components/QuizPage';
 import HomePage from './components/HomePage';
 import NotFoundPage from './components/NotFoundPage';
 import GlobalStyles from './styles/GlobalStyles';
+import {Helmet} from "react-helmet"
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>React Kyryll's App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<HomePage />} />
